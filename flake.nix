c36a77b4a0f96e791516cd1ef20e3afd281f9138
S-tatsuya <s.tatsuya0123@gmail.com>
2025-01-25 21:16:06 +0900

    chore: unFreeを有効にした

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       myHome = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = import inputs.nixpkgs {
 	  system = "x86_64-linux";
-	  # config.allowUnfree = true;
+	  config.allowUnfree = true;
 	};
 	extraSpecialArgs = {
 	  inherit inputs;
